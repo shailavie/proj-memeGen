96d0b309a3578eca6bbf79cc968a176844cb3537
Shai Lavie <shai.lavie@gmail.com>
2019-02-17 14:48:58 +0200

    fb share #4

--- js/editor.js
+++ js/editor.js
@@ -481,6 +481,34 @@ function uploadImg(elForm, ev) {
 }
 
 
+function doUploadImg(elForm, onSuccess) {
+    var formData = new FormData(elForm);
+
+    fetch('http://ca-upload.com/here/upload.php', {
+        method: 'POST',
+        body: formData
+    })
+    .then(function (response) {
+        return response.text()
+    })
+    .then(onSuccess)
+    .catch(function (error) {
+        console.error(error)
+    })
+}
+
+
+
+
+// facebook api
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/he_IL/sdk.js#xfbml=1&version=v3.0&appId=807866106076694&autoLogAppEvents=1';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
 // var canvas = document.getElementById('myCanvas');
 // var ctx = canvas.getContext('2d');
 
